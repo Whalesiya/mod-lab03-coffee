5a9e479f806ebb5065630f669c66d0af69c666c1
Whalesiya <100674297+Whalesiya@users.noreply.github.com>
2022-04-03 21:34:45 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -5,7 +5,7 @@
 #include <string>
 #include <map>
 enum States {
-OFF, ACCEPT, WAIT, CHOSEN, MAKING, READY
+OFF, ACCEPT, WAIT, CHECK, COOK
 };
 
 class Automata {
@@ -19,22 +19,25 @@ Automata() {
 this->state = OFF;
 this->cash = 0;
 this->choice = -1;
-menu[0] = "Латте";
-menu[1] = "Капучино";
-menu[2] = "Эспрессо";
-prices[0] = 50;
-prices[1] = 60;
-prices[2] = 70;
+menu[0] = "Горячий шоколад";
+menu[1] = "Двойной шоколад";
+menu[2] = "Какао";
+menu[3] = "Холодный кофе";
+menu[4] = "Горячий кофе";
+menu[5] = "Холодный чай";
+prices[0] = 200;
+prices[1] = 300;
+prices[2] = 100;
+prices[3] = 180;
+prices[4] = 150;
+prices[5] = 500;
 }
 void on();
 void off();
-void coin();
 void coin(int);
 void etMenu(std::map <int, std::string>, std::map <int, int>);
-void etMenu(std::string, std::string);
 States getState();
 int getCash();
-void choiceuser();
 void choiceuser(int);
 void check();
 void cancel();
