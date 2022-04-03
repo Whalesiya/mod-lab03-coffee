f75a22adef490b59bc97cb54bc0fbca6fcc2d9f5
Whalesiya <100674297+Whalesiya@users.noreply.github.com>
2022-04-03 21:41:47 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,57 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+TEST(TEST1, TrueOrFalse) {
+    Automata automata;
+    automata.on();
+    EXPECT_EQ(States::WAIT, automata.getState());
+}
+
+TEST(TEST2, TrueOrFalse) {
+    Automata automata;
+    automata.on();
+    automata.coin(250);
+    automata.coin(300);
+    EXPECT_EQ(States::ACCEPT, automata.getState());
+}
+
+TEST(TEST3, TrueOrFalse) {
+    Automata automata;
+    automata.on();
+    automata.coin(250);
+    automata.coin(300);
+    automata.choiceuser(3);
+    EXPECT_EQ(States::ACCEPT, automata.getState());
+}
+
+TEST(TEST4, TrueOrFalse) {
+    Automata automata;
+    automata.on();
+    automata.coin(250);
+    automata.coin(300);
+    automata.choiceuser(3);
+    automata.check();
+    EXPECT_EQ(States::COOK, automata.getState());
+}
+
+TEST(TEST5, TrueOrFalse) {
+    Automata automata;
+    automata.on();
+    automata.coin(800);
+    automata.choiceuser(5);
+    automata.check();
+    automata.cook();
+    EXPECT_EQ(300, automata.getCash());
+}
+
+TEST(TEST6, TrueOrFalse) {
+    Automata automata;
+    automata.on();
+    automata.coin(800);
+    automata.choiceuser(5);
+    automata.check();
+    automata.cook();
+    automata.finish();
+    automata.off();
+    EXPECT_EQ(States::OFF, automata.getState());
+}
