2a95c978efaf2ba6cff757d24d9e9f5018cf8fa8
Whalesiya <100674297+Whalesiya@users.noreply.github.com>
2022-04-03 21:34:54 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -12,19 +12,8 @@ this->state = WAIT;
 void Automata::off() {
 this->state = OFF;
 }
-void Automata::coin() {
-if (this->state != OFF) {
-int money = 0;
-std::cout << "Введите сумму: ";
-std::cin >> money;
-this->coin(money);
-}
-}
 void Automata::coin(int money) {
 if (this->state != OFF) {
-if (money < 0)
-this->cash += -money;
-else
 this->cash += money;
 this->state = ACCEPT;
 }
@@ -44,62 +33,26 @@ this->prices[this->prices.size()] = 50;
 }
 }
 }
-void Automata::etMenu(std::string pathmenu, std::string pathprices) {
-std::ifstream fin;
-fin.open(pathmenu);
-this->menu.clear();
-this->prices.clear();
-if (!fin.is_open()) {
-std::cout << "Ошибка открытия файла\n";
-} else {
-while (!fin.eof()) {
-std::getline(fin, this->menu[this->menu.size()]);
-}
-}
-fin.close();
-fin.open(pathprices);
-if (!fin.is_open()) {
-std::cout << "Ошибка открытия файла\n";
-} else {
-while (!fin.eof()) {
-std::getline(fin, this->menu[this->menu.size()]);
-this->prices[this->prices.size()] = std::stoi(this->menu[this->menu.size()-1]);
-this->menu.erase(this->menu.size());
-}
-}
-if (this->prices.size() < this->menu.size()) {
-for (int i = 0; i < this->menu.size() - this->prices.size(); i++) {
-this->prices[this->prices.size()] = 50;
-}
-}
-}
 States Automata::getState() {
 return this->state;
 }
 int Automata::getCash() {
 return this->cash;
 }
-void Automata::choiceuser() {
-int choice = 0;
-std::cin >> choice;
-this->choiceuser(choice);
-}
 void Automata::choiceuser(int choice) {
-if (this->state == ACCEPT) {
-if (choice<0 || choice>this->menu.size()) {
-this->state = WAIT;
-} else {
+if (this->state != OFF) {
 this->choice = choice;
-this->state = CHOSEN;
-}
+this->state = ACCEPT;
+} else {
+this->state = WAIT;
 }
 }
 void Automata::check() {
-if (this->state == CHOSEN) {
+if (this->state == ACCEPT) {
 if (this->cash < this->prices[this->choice]) {
 this->state = WAIT;
 } else {
-this->state = MAKING;
+this->state = COOK;
 }
 } else {
 this->state = WAIT;
@@ -111,9 +64,9 @@ this->state = WAIT;
 }
 }
 void Automata::cook() {
-if (this->state == MAKING) {
+if (this->state == COOK) {
 this->cash -= this->prices[this->choice];
-this->state = READY;
+this->state = WAIT;
 }
 }
 void Automata::finish() {
